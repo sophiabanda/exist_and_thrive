155352eb1ff1cc5eebe2434048457e47706f873c
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-06-23 13:43:27 -0400

    sticky nav, wip

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// alert('Hello!');
+window.onscroll = function () {
+  stickyNav();
+};
+
+const navbar = document.getElementById('navbar');
+const sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  }
+}
